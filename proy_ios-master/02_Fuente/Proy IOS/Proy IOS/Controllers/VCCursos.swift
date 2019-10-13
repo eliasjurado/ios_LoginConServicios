faa5ae79d2748b7524e0adf923e78becf60a75b7
Elías Luis Jurado Santos <eliasljurado@gmail.com>
2019-10-13 10:31:49 -0500

    Carga inicial de archivos

---
+++ proy_ios-master/02_Fuente/Proy IOS/Proy IOS/Controllers/VCCursos.swift
@@ -0,0 +1,32 @@
+//
+//  VCCursos.swift
+//  Proy IOS
+//
+//  Created by P on 10/7/19.
+//  Copyright © 2019 JSTech. All rights reserved.
+//
+
+import UIKit
+
+class VCCursos: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//cambiar un valor en otro controlador
+        //let vTab = self.storyboard?.instantiateViewController(withIdentifier: "UIAsesoria") as! VCProfesionales
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
